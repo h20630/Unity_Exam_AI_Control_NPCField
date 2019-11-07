f87f7471aabdfd89285348efc31e4457532ff09e
h20630 <57204868+h20630@users.noreply.github.com>
2019-11-07 11:23:16 +0800

    B1040401

---
+++ UnityProject/Assets/NewBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Rigidbody go;
+    [Header("速度")]
+    [Range(3, 120)]
+    public float speed = 3;
+    [Header("跳躍")]
+    [Range(3, 120)]
+    public float jump = 3;
+    [Header("是否接觸地面")]
+    public bool isGround;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        go = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+        Debug.Log("touch" + collision.gameObject);
+    }
+
+    private void Walk()
+    {
+        go.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, 0));
+        go.AddForce(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            go.AddForce(new Vector3(0, jump, 0));
+        }
+
+    }
+}
---
+++ UnityProject/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    [Header("對話")]
+    public string Say = "走開啦~";
+    public float SaySpeed = 1f;
+    [Header("任務")]
+    public bool Complete = false;
+    public int BateryFinish = 10;
+    public int Batery = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
